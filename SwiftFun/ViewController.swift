154bc46ffbb71c85cabb8c1be1fe317e48b66b88
Joe Moriguchi <guchfamily@Joes-MacBook-Air.local>
2017-08-01 20:46:50 -0700

    this is my first Git Save!

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    @IBOutlet var myLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 10 {
+            
+            view.backgroundColor = UIColor.red
+            
+            myLabel.text = "You hit the button more than 10 times!"
+        
+            
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
+        
+        
+ 
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
