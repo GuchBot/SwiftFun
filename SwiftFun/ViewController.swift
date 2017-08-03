53e71ab8bb82e471ecd9ba1ead66fd2f99a3e676
Unknown <guchfunmail@gmail.com>
2017-08-03 00:05:25 -0700

    Now You can Add or Subtract!
    janky Calc

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -14,8 +14,33 @@ class ViewController: UIViewController {
     
     @IBOutlet var myLabel: UILabel!
     
+    @IBOutlet var topTextField: UITextField!
+    
+    @IBOutlet var bottomTextField: UITextField!
+    
+    @IBOutlet var additionSwitch: UISwitch!
+    
     @IBAction func buttonTapped(_ sender: Any) {
+
+        let addition = additionSwitch.isOn
         
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+            
+        } else {
+            
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+            
+        }
+        
+        
+        
+        
+        /*
         buttonCount = buttonCount + 1
         
         print(buttonCount)
@@ -29,7 +54,7 @@ class ViewController: UIViewController {
             
         }
         
-        
+      */
         
     }
     
